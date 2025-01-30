5dbd014f7f5f6519adcac743b5ba86e1092be070
Pedro Luis <zPedroLuis@outlook.com>
2025-01-30 17:00:13 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
+import database from "../../../../infra/database.js";
+
 //Request trata o que lá do mundo de fora ta entrando dentro do seu sistema
 //Response trata o que você quer responder lá pra fora
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
 
